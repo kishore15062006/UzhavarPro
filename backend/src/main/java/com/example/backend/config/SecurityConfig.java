e4b54da46446219c4de3b2c1ccbe1740e052f7db
kishore15062006 <kishore15062006@gmail.com>
2026-07-06 21:19:08 +0530

    6th july final commit

--- backend/src/main/java/com/example/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173","http://localhost:3000","https://uzhavarpro.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
